0d7295e7a811086151a9e226c077d2cef4ab09f3
Eddy <eddymutugik@gmail.com>
2025-12-19 10:13:57 +0300

    Fix formatting in views.py import statements

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Avg, Count, Sum
 from django.utils import timezone
